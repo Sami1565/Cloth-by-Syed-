1d5fd07a72167a1c7ca4348969d30753b2e2e5d8
Sami1565 <Syedsamad1565@gmail.com>
2026-07-16 14:16:51 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,19 +3,11 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
-}
-
-module.exports = nextConfig
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
   webpack: (config) => {
     config.externals = [...config.externals, 'canvas', 'jsdom']
     return config
   },
-  transpilePackages: ['three'],
 }
 
 module.exports = nextConfig
